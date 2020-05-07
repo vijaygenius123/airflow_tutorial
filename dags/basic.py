2bad7b064ebc2f4aee2ee84607c87eb6bb135c3c
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-05-07 18:46:15 +0530

    Basic DAG

---
+++ dags/basic.py
@@ -0,0 +1,27 @@
+from datetime import timedelta
+
+import airflow
+from airflow import DAG
+from airflow.operators import BashOperator
+
+
+default_args = {
+    'owner':'airflow',
+    'start_date': airflow.utils.dates.days_ago(1),
+    'depends_on_past': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5)
+}
+
+dag = DAG(
+    'example',
+    default_args=default_args,
+    description='Simple DAG',
+    schedule_interval=timedelta(days=1)
+)
+
+t1 = BashOperator(
+    task_id='print_date',
+    bash_command='date',
+    dag=dag,
+)
